956185a024d96a8d05b5455e30066babdce5c4ea
Nathan Standage <developer@nathanstandage.com>
2018-05-13 21:30:15 -0600

    Refactoring/consolidating the backend. Deleted the other files I didn't need.

---
+++ backend.js
@@ -0,0 +1,181 @@
+//var    getCompliantItems("Shells", 2, 18);
+function getCompliantItems(sheetName, row, column) {
+    var sheet = getSheet(sheetName);
+    var compliantItems = [];
+    var nameColumn = 1;
+    var nameCell = sheet.getRange(row, nameColumn); // first nameColumn
+    while (nameCell.getValue() != "") {
+        if (sheet.getRange(row, column).getValue() == "Yes") {
+            compliantItems.push(sheet.getRange(row, nameColumn).getValue());
+        }
+        row++;
+        nameCell = sheet.getRange(row, nameColumn);
+    }
+    console.log("Compliant Items: " + compliantItems);
+    return compliantItems;
+}
+
+function getSheet(sheetName) {
+    return SpreadsheetApp.openById("1-sSObYwouUu8Y7gPFmNcGhIy2PDTjfREuLLrdtVrx9U").getSheetByName(sheetName);
+}
+
+function linkTimeToDate(rawArray) {
+    console.log("linkTimeToDate");
+    var dateTimeOut = new Date(rawArray[1]);
+    const timeBlockOut = rawArray[2];
+    var dateTimeIn = new Date(rawArray[3]);
+    const timeBlockIn = rawArray[4];
+    var databaseReadyArray = [rawArray[0]];
+    switch (timeBlockOut) {
+        case 'Early Morning':
+            dateTimeOut.setHours(11);
+            break;
+        case 'Morning':
+            dateTimeOut.setHours(13);
+            break;
+        case 'Afternoon':
+            dateTimeOut.setHours(15);
+            break;
+        case 'Late Afternoon':
+            dateTimeOut.setHours(17);
+            break;
+    }
+    switch (timeBlockIn) {
+        case 'Early Morning':
+            dateTimeIn.setHours(11);
+            break;
+        case 'Morning':
+            dateTimeIn.setHours(13);
+            break;
+        case 'Afternoon':
+            dateTimeIn.setHours(15);
+            break;
+        case 'Late Afternoon':
+            dateTimeIn.setHours(17);
+            break;
+    }
+    databaseReadyArray.push(dateTimeOut);
+    databaseReadyArray.push(dateTimeIn);
+    databaseReadyArray.push(rawArray[5]);
+    databaseReadyArray.push(rawArray[6]);
+    databaseReadyArray.push(rawArray[7]);
+
+    return databaseReadyArray;
+}
+
+function getScheduledDates() {
+    var row = 2;
+    var column = 1;
+    var scheduledDates = [];
+    var backendSheet = SpreadsheetApp.openById("1-sSObYwouUu8Y7gPFmNcGhIy2PDTjfREuLLrdtVrx9U").getSheetByName("Backend");
+
+    //iterate through data and add to selectedDates
+    while (backendSheet.getRange(row, column).getValue() != "") {
+        scheduledDates.push(getScheduledDate(row, column, backendSheet));
+        row++;
+    }
+    return scheduledDates;
+}
+
+function getScheduledDate(row, column, sheet) {
+    var dateArray = [];
+    for (i = 0; i < 6; i++) {
+        dateArray.push(sheet.getRange(row, column + i).getValue());
+    }
+    return dateArray;
+}
+
+function removeExpiredData() {
+    var scheduledDates = getScheduledDates();
+    var yesterday = new Date(Date.now() - 86400000);
+    for (i = 0; i < scheduledDates.length; i++) {
+        var date = new Date(scheduledDates[i][2]);
+        if (date < yesterday) {
+            delete scheduledDates[i];
+        } else {
+        }
+    }
+    writeScheduledDates(scheduledDates);
+}
+
+function writeScheduledDates(scheduledDates) {
+    var sheet = SpreadsheetApp.openById("1-sSObYwouUu8Y7gPFmNcGhIy2PDTjfREuLLrdtVrx9U").getSheetByName("Backend");
+    var row = 2;
+    var column = 1;
+    wipeScheduledDates();
+    for (j = 0; j < scheduledDates.length; j++) {
+        if (scheduledDates[j] != undefined) {
+            writeDate(row, column, scheduledDates[j], sheet);
+            row++;
+        }
+    }
+}
+
+function writeDate(row, column, scheduledDate, sheet) {
+    for (i = 0; i < scheduledDate.length; i++) {
+        sheet.getRange(row, column + i).setValue(scheduledDate[i]);
+    }
+}
+
+function wipeScheduledDates() {
+    SpreadsheetApp.openById("1-sSObYwouUu8Y7gPFmNcGhIy2PDTjfREuLLrdtVrx9U").getSheetByName("Backend").getRange(2, 1, 500, 6).setValue("");
+}
+
+function checkAvailability(proposedDate) {
+    var scheduledDates = getScheduledDates(); // This should grab the dates that are already in the calendar
+    var compliantShells = getCompliantItems("Shells", 2, 18);
+    var unavailableDates = [];
+    var availableShells = [];
+    //check propsed out date against scheduled in dates
+    for (i = 0; i < scheduledDates.length; i++) {
+        if (proposedDate[1] < scheduledDates[i][2]) {
+            if (proposedDate[1] > scheduledDates[i][1]) {
+                unavailableDates.push(scheduledDates[i]);
+            }
+        }
+    }
+    //check proposed in date against scheduled out Dates. save overlaps in array
+    for (j = 0; j < scheduledDates.length; j++) {
+        if (proposedDate[3] > scheduledDates[j][1]) {
+            if (proposedDate[3] < scheduledDates[j][2]) {
+                unavailableDates.push(scheduledDates[j]);
+            }
+        }
+    }
+    // check unavailable dates against compliant shells
+    for (k = 0; k < unavailableDates.length; k++) {
+        for (l = 0; l < compliantShells.length; l++) {
+            if (unavailableDates[k][4] == compliantShells[l]) {
+                compliantShells[l] = null;
+            }
+        }
+    }
+    //transfer compliant shells to available shells.
+    for (m = 0; m < compliantShells.length; m++) {
+        if (compliantShells[m] != null) {
+            availableShells.push(compliantShells[m]);
+        }
+    }
+    return availableShells;
+}
+
+function saveScheduledDate(rawData) {
+    var dateToWrite = linkTimeToDate(rawData);
+    var row = 2;
+    const column = 1;
+    var i = 0;
+    while (SpreadsheetApp.openById("1-sSObYwouUu8Y7gPFmNcGhIy2PDTjfREuLLrdtVrx9U").getSheetByName("Backend").getRange(row + i, column).getValue() != "") {
+        i++;
+    }
+    writeDate(row + i, column, dateToWrite, SpreadsheetApp.openById("1-sSObYwouUu8Y7gPFmNcGhIy2PDTjfREuLLrdtVrx9U").getSheetByName("Backend"));
+}
+
+function doGet(request) {
+    return HtmlService.createTemplateFromFile('index')
+        .evaluate();
+}
+
+function include(filename) {
+    return HtmlService.createHtmlOutputFromFile(filename)
+        .getContent();
+}
--- frontEndScripts.js
+++ frontEndScripts.js
@@ -1,16 +1,15 @@
-let clientNameTextbox = document.getElementById('clientNameTextbox');
-let outDateTextbox = document.getElementById('outDateTextbox');
-let outTimeDropdown = document.getElementById('outTimeDropdown');
-let inDateTextbox = document.getElementById('inDateTextbox');
-let inTimeDropdown = document.getElementById('inTimeDropdown');
-let coordinatorTextbox = document.getElementById('coordinatorTextbox');
-let shellsDropdown = document.getElementById('shellsDropdown');
-let trucksDropdown = document.getElementById('trucksDropdown');
-let checkAvailabilityButton = document.getElementById('checkAvailabilityButton');
-let scheduleButton = document.getElementById('scheduleButton');
+function checkAvailability() {
+    // add if statement that checks all data is entered
+    const clientNameTextbox = document.getElementById('clientNameTextbox');
+    const outDateTextbox = document.getElementById('outDateTextbox');
+    const outTimeDropdown = document.getElementById('outTimeDropdown');
+    const inDateTextbox = document.getElementById('inDateTextbox');
+    const inTimeDropdown = document.getElementById('inTimeDropdown');
+    const coordinatorTextbox = document.getElementById('coordinatorTextbox');
+    var shellsDropdown = document.getElementById('shellsDropdown');
+    var trucksDropdown = document.getElementById('trucksDropdown');
+    var scheduleButton = document.getElementById('scheduleButton');
 
-
-checkAvailabilityButton.addEventListener('click', function() {
     var dataToCheck = [];
     dataToCheck.push(clientNameTextbox.value);
     dataToCheck.push(outDateTextbox.value);
@@ -19,24 +18,109 @@ checkAvailabilityButton.addEventListener('click', function() {
     dataToCheck.push(inTimeDropdown.value);
     dataToCheck.push(coordinatorTextbox.value);
     
-    console.log(dataToCheck);
-});
+    google.script.run
+        .withSuccessHandler(function (availableShells) {
+            console.log("Success1");
+            for (i = 0; i < availableShells.length; i++) {
+                var shellOption = document.createElement('option');
+                shellOption.text = availableShells[i];
+                shellsDropdown.add(shellOption);
+            }
 
-scheduleButton.addEventListener('click', function() {
-    var dataToCheck = [];
-    dataToCheck.push(clientNameTextbox.value);
-    dataToCheck.push(outDateTextbox.value);
-    dataToCheck.push(outTimeDropdown.value);
-    dataToCheck.push(inDateTextbox.value);
-    dataToCheck.push(inTimeDropdown.value);
-    dataToCheck.push(coordinatorTextbox.value);
-    dataToCheck.push(trucksDropdown.value);
-    dataToCheck.push(shellsDropdown.value);
+//             var min = 12,
+//     max = 100,
+//     select = document.getElementById('selectElementId');
 
-    console.log(dataToCheck);
+// for (var i = min; i<=max; i++){
+//     var opt = document.createElement('option');
+//     opt.value = i;
+//     opt.innerHTML = i;
+//     select.appendChild(opt);
+// }
+        })
+        .withFailureHandler(function (error) {
+            console.log("Failure1");
+            //create popup thaat displays text instead of message.
+            console.log("Couldn't check availability. Please check fields and try again.");
+            console.log(error);
+        })
+        .checkAvailability(dataToCheck);
 
-});
+    google.script.run
+        .withSuccessHandler(function(compliantTrucks) {
+            console.log("Success2");
+            for (i = 0; i < compliantTrucks.length; i++) {
+                var truckOption = document.createElement("option");
+                truckOption.text = compliantTrucks[i];
+                trucksDropdown.add(truckOption);
+            }
+        })
 
+        .withFailureHandler(function(error) {
+            console.log("Failure2");
+            console.log("Couldn't check availability. Please check fields and try again.@@@@");
+            console.log(error);
+
+        })
+
+        .getCompliantItems("Trucks", 2, 19);
+
+
+
+        console.log("FINISHED!");
+}
+
+function saveToSchedule() {
+    //add if statement that checks all data is entered
+    const clientNameTextbox = document.getElementById('clientNameTextbox');
+    const outDateTextbox = document.getElementById('outDateTextbox');
+    const outTimeDropdown = document.getElementById('outTimeDropdown');
+    const inDateTextbox = document.getElementById('inDateTextbox');
+    const inTimeDropdown = document.getElementById('inTimeDropdown');
+    const coordinatorTextbox = document.getElementById('coordinatorTextbox');
+    var shellsDropdown = document.getElementById('shellsDropdown');
+    var trucksDropdown = document.getElementById('trucksDropdown');
+
+
+    var dataToWrite = [];
+    dataToWrite.push(clientNameTextbox.value);
+    dataToWrite.push(outDateTextbox.value);
+    dataToWrite.push(outTimeDropdown.value);
+    dataToWrite.push(inDateTextbox.value);
+    dataToWrite.push(inTimeDropdown.value);
+    dataToWrite.push(trucksDropdown.value);
+    dataToWrite.push(shellsDropdown.value);
+    dataToWrite.push(coordinatorTextbox.value);
+
+    google.script.run
+    .withSuccessHandler(function(){
+    console.log("WE DID IT")
+    removeOldSelectItems();
+    
+    })
+    .withFailureHandler(function(error) {
+    console.log(error);
+    removeOldSelectItems();
+    })
+    .saveScheduledDate(dataToWrite);
+
+}
+
+
+function removeOldSelectItems() {
+    var shellsDropdown = document.getElementById('shellsDropdown');
+    var trucksDropdown = document.getElementById('trucksDropdown');
+
+    while (shellsDropdown.length > 1) {
+        shellsDropdown.remove(shellsDropdown.length-1);
+    }
+
+    while (trucksDropdown.length > 1) {
+        trucksDropdown.remove(trucksDropdown.length-1);
+    }
+
+
+}
 
 
 
